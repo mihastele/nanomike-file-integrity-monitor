f4ddaade3bd11ed9091dd5abf4ea495660513fd7
Miha Stele <stele1000@gmail.com>
2026-09-20 16:43:45 +0200

    Optimize imports

--- src/main/java/com/mihastele/core/FileIntegrityMonitor.java
+++ src/main/java/com/mihastele/core/FileIntegrityMonitor.java
@@ -12,7 +12,6 @@ import java.security.NoSuchAlgorithmException;
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class FileIntegrityMonitor {
